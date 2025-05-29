028fe72060389f70ffb3ada51e99ea5ba4a79a1c
Mateusz Gancarz <grzmateusz6@gmail.com>
2025-05-29 20:22:55 +0200

    project: remove unused scripts

--- Scripts/BuildDebug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BUILD_TYPE="Debug"
-DEBUG_BUILD_DIR="Debug"
-
-mkdir -p $DEBUG_BUILD_DIR
-cmake -B $DEBUG_BUILD_DIR -S . -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE
-cmake --build $DEBUG_BUILD_DIR --target chaos-in-world-generation -j $(nproc --all)
--- Scripts/BuildRelease.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BUILD_TYPE="Release"
-BUILD_DIR="Release"
-
-mkdir -p $BUILD_DIR
-cmake -B $BUILD_DIR -S . -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE
-cmake --build $BUILD_DIR --target chaos-in-world-generation -j $(nproc --all)
